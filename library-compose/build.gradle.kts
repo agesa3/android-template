878e7eafc1d0d168c89118329ee8e8f3eb2e9d07
Agesa Collins <47299413+agesa3@users.noreply.github.com>
2022-08-20 15:40:24 +0300

    Initial commit

---
+++ library-compose/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = libs.versions.compile.sdk.version.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.min.sdk.version.get().toInt()
+        targetSdk = libs.versions.target.sdk.version.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    buildFeatures {
+        compose = true
+        viewBinding = true
+        buildConfig = false
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compilerextension.get()
+    }
+
+    lint {
+        warningsAsErrors = true
+        abortOnError = true
+    }
+}
+
+dependencies {
+    implementation(project(":library-kotlin"))
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.material)
+
+    testImplementation(libs.junit)
+    
+    debugImplementation(libs.compose.ui.test.manifest)
+    androidTestImplementation(libs.compose.ui.test.junit4)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+}
